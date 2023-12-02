e6c02e340515e1a8afa7490c2673877432de6aa1
Nifel13 <eduard.barnadas@estudiantat.upc.edu>
2023-12-02 21:44:08 +0100

    Refactor code to improve heuristic calculation

--- journeyAdria.py
+++ journeyAdria.py
@@ -40,8 +40,31 @@ def use_token(origin, destination, date, time):
 best_destinations = []
 
 for i in range(len(use_token("8503000", "8507000", "2023-04-18", "13:07")['trips'])):
-    best_destinations.append(use_token("8503000", "8507000", "2023-04-18", "13:07")['trips'][i]['duration'])
+    best_destinations.append(use_token("8503000", "8507000", "2023-04-18", "13:07")['trips'][i])
+    print(use_token("8503000", "8507000", "2023-04-18", "13:07")['trips'][i]["duration"])
+    print(len(use_token("8503000", "8507000", "2023-04-18", "13:07")['trips'][i]["legs"]))
 
-print(best_destinations)
 
 
+def heuristic(dest_list: list):
+    best_train = [None,999999999]
+    for viatge in dest_list:
+        puntuacio = 0
+        puntuacio += hour_to_min(viatge)
+        puntuacio += len(viatge['legs'])*10
+        if puntuacio < best_train[1]:
+            best_train[0] = viatge
+            best_train[1] = puntuacio
+    print('heuristic points: ',best_train[1], "time:" ,hour_to_min(best_train[0]), "Transfers: ", len(best_train[0]['legs']))
+    return best_train[0]
+        
+
+def hour_to_min(travel):
+    duration = travel['duration']
+    parts = duration[2:].split('H')
+    hours = int(parts[0]) if len(parts) > 1 else 0
+    minutes = int(parts[1][:-1]) if len(parts) > 1 else 0
+    return hours * 60 + minutes
+    
+
+heuristic(best_destinations)
