95904ba9d170da35537328b9f93cc5a6408cf189
Barrenee <barrenecheapereapablo@gmail.com>
2023-12-03 03:30:46 +0100

    fvwe

--- WebMap/main.py
+++ WebMap/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/WebMap")
 def hello_world():
-    return render_template("index.html", bitch_var="Hello World")
+    return render_template("index.html", bitch_var="Yes BITCHHH")
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
